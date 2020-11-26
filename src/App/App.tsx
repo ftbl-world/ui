6473bcc3a61e74182a6b5145510baf929f4ab3e0
Rahul Akarapu <rahul.akarapu@sap.com>
2020-11-26 20:22:26 +0530

    Implement basic routing

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,34 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "../Header/Header";
 import Score from "../Score/Score";
 import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Score />
-    </div>
+    <BrowserRouter>
+      <div id="wrapper">
+        <Switch>
+          <Route path="/home" render={() => (
+            <div className="App">
+              <Header />
+          </div>
+          )} />
+          <Route path="/score" render={() => (
+            <div className="App">
+              <Header />
+              <Score />
+          </div>
+          )} />
+          <Route path="/" render={() => (
+            <div className="App">
+              <Header />
+              <Score />
+          </div>
+          )} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
