0165ef6ee3ede3940ae7043c34223e1e3264bc64
ravimashru <me@ravimashru.dev>
2021-05-23 14:06:48 +0530

    FIX: single digit 0 in minutes

--- src/Score/Score.tsx
+++ src/Score/Score.tsx
@@ -49,7 +49,8 @@ function Live() {
   const convertTime = (timeString: string) => {
     const date = new Date(timeString);
     const hours = ('0' + date.getHours()).slice(-2);
-    const minutes = date.getMinutes();
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+;
     return `${hours}:${minutes}`;
   }
 
